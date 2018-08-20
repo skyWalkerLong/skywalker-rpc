fbae8f61c84f31b6f142b67fb7259e9138c264f5
longchao <longchao@jd.com>
2018-08-20 11:31:33 +0800

    common push

---
+++ rpc-common/src/main/java/com/skywalker/rpc/po/RpcResponse.java
@@ -0,0 +1,8 @@
+package com.skywalker.rpc.po;
+
+/**
+ * @author longchao
+ * @date 2018/8/20.
+ */
+public class RpcResponse {
+}
---
+++ rpc-common/src/main/java/com/skywalker/rpc/serialize/RpcDecode.java
@@ -0,0 +1,8 @@
+package com.skywalker.rpc.serialize;
+
+/**
+ * @author longchao
+ * @date 2018/8/20.
+ */
+public class RpcDecode {
+}
