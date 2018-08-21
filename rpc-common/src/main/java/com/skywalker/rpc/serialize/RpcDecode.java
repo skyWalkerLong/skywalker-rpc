5f8c6ac86cb1cc350d58d63d4c5641af834692c9
longchao <longchao@jd.com>
2018-08-21 18:48:38 +0800

    common push

--- rpc-common/src/main/java/com/skywalker/rpc/serialize/RpcDecode.java
+++ rpc-common/src/main/java/com/skywalker/rpc/serialize/RpcDecode.java
@@ -1,8 +1,47 @@
 package com.skywalker.rpc.serialize;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
 /**
+ * 解码器
  * @author longchao
  * @date 2018/8/20.
  */
-public class RpcDecode {
+public class RpcDecode extends ByteToMessageDecoder {
+
+    /**
+     * 解码模板
+     */
+    private Class<?> decodeClass;
+
+    private static final Integer length = 4;
+
+    public RpcDecode(Class<?> decodeClass) {
+        this.decodeClass = decodeClass;
+    }
+
+    @Override
+    public final void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        if (in.readableBytes() < length) {
+            return;
+        }
+
+        in.markReaderIndex();
+        int dataLength = in.readInt();
+        if (dataLength < 0) {
+            ctx.close();
+        }
+        if (in.readableBytes() < dataLength) {
+            in.resetReaderIndex();
+        }
+        byte[] data = new byte[dataLength];
+        in.readBytes(data);
+
+        Object obj =
+
+    }
 }
