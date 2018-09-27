e38192048a80f66e15174d4b968c32c12f6c05bb
skyWalkerLong <scu_longchao@163.com>
2018-09-27 12:06:34 +0800

    dingyi

--- rpc-common/src/main/java/com/skywalker/rpc/po/RpcRequest.java
+++ rpc-common/src/main/java/com/skywalker/rpc/po/RpcRequest.java
@@ -1,8 +1,21 @@
 package com.skywalker.rpc.po;
 
+import lombok.Data;
+
 /**
  * @author longchao
  * @date 2018/8/20.
  */
+@Data
 public class RpcRequest {
+
+    private String requestId;
+
+    private String className;
+
+    private String methodName;
+
+    private Class<?>[] parameterTypes;
+
+    private Object[] parameters;
 }
--- rpc-common/src/main/java/com/skywalker/rpc/po/RpcResponse.java
+++ rpc-common/src/main/java/com/skywalker/rpc/po/RpcResponse.java
@@ -1,8 +1,17 @@
 package com.skywalker.rpc.po;
 
+import lombok.Data;
+
 /**
  * @author longchao
  * @date 2018/8/20.
  */
+@Data
 public class RpcResponse {
+
+    private String requestId;
+
+    private Throwable error;
+
+    private Object result;
 }
