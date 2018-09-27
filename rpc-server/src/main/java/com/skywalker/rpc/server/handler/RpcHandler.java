8ccb94487fb3ba3c38f9edff3a2f8689ede84897
skyWalkerLong <scu_longchao@163.com>
2018-09-27 15:43:09 +0800

    服务端处理

--- rpc-server/src/main/java/com/skywalker/rpc/server/handler/RpcHandler.java
+++ rpc-server/src/main/java/com/skywalker/rpc/server/handler/RpcHandler.java
@@ -1,11 +1,14 @@
 package com.skywalker.rpc.server.handler;
 
 import com.skywalker.rpc.po.RpcRequest;
+import com.skywalker.rpc.po.RpcResponse;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -22,8 +25,35 @@ public class RpcHandler extends SimpleChannelInboundHandler<RpcRequest> {
     }
 
     @Override
-    public void messageReceived(final ChannelHandlerContext ctx, RpcRequest request) {
+    public void messageReceived(final ChannelHandlerContext ctx, RpcRequest request) throws Exception{
+        RpcResponse response = new RpcResponse();
+        response.setRequestId(request.getRequestId());
+        try {
+            Object result = handle(request);
+            response.setResult(result);
+        } catch (Throwable t) {
+            response.setError(t);
+        }
 
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * 具体调用
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    private Object handle(RpcRequest request) throws Exception {
+        String className = request.getClassName();
+        Object serviceBean = handlerMap.get(className);
+
+        String methodName = request.getMethodName();
+        Class<?>[] parameterTypes = request.getParameterTypes();
+        Object[] parameters = request.getParameters();
+        Class<?> interfaceName = Class.forName(className);
+        Method method = interfaceName.getMethod(methodName,parameterTypes);
+        return method.invoke(serviceBean,parameters);
     }
 
     @Override
